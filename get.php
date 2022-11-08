5ae99bcb15089b0bf2c3b6707911188ec62dccbc
Tanel Märjama <tanel.marjama@khk.ee>
2022-11-08 18:28:17 +0200

    Repaired delete and get scripts

--- get.php
+++ get.php
@@ -1,1 +1,21 @@
 <?php
+include_once 'database.php';
+include_once 'todos.php';
+$database = new Database();
+
+$db = $database->getConnection();
+$items = new todos($db);
+$records = $items->getToDos();
+$itemCount = $records->num_rows;
+if($itemCount > 0){
+    $toDoArray = array();
+    while ($row = $records->fetch_assoc())
+    {
+        array_push($toDoArray, $row);
+    }
+    echo json_encode($toDoArray);
+}
+else{
+    http_response_code(204);
+}
+?>
